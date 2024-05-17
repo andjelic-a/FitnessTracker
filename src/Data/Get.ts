3fa56381bb4d4790296d830c1e5fed4bd96a39e8
Andrej Nenadić <andrejnana445@gmail.com>
2024-05-17 21:59:59 +0200

    Finished Get<T> method

--- src/Data/Get.ts
+++ src/Data/Get.ts
@@ -1,9 +1,6 @@
-import Exercise from "../Types/Models/Exercise";
 import IModel from "../Types/Models/IModel";
 
 const BaseAPIUrl = "http://192.168.1.100:5054/api";
-
-Get<Exercise>("aliases");
 
 type Include<T extends IModel> = {
   [P in keyof T as T[P] extends any[] ? P : never]: T[P];
@@ -13,9 +10,30 @@ type IncludeKeys<T extends IModel> = keyof Include<T>;
 
 type Query<T extends IModel> = `${keyof Include<T>}=${string}`;
 
-Get<Exercise>("images", "primaryMuscles=1,5,12");
-
 export default async function Get<T extends IModel>(
-  include: IncludeKeys<T> | IncludeKeys<T>[],
-  q?: Query<T> | Query<T>[]
-) {}
+  apiEndpoint: string,
+  include?: IncludeKeys<T> | IncludeKeys<T>[],
+  q?: Query<T> | Query<T>[],
+  limit: number = 10,
+  offset: number = 0
+): Promise<T[]> {
+  const includeString = !include
+    ? undefined
+    : Array.isArray(include)
+    ? include.join(",")
+    : include;
+
+  const queryString = !q ? undefined : Array.isArray(q) ? q.join(";") : q;
+
+  const result = await fetch(
+    `${BaseAPIUrl}/${apiEndpoint}?include=${
+      includeString ? includeString : "none"
+    }&${queryString ? queryString : ""}&limit=${limit}&offset=${offset}`,
+    {
+      method: "GET",
+    }
+  );
+
+  const json = await result.json();
+  return json as T[];
+}
