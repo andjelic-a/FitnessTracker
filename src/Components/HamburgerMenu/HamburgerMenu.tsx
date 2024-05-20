d9a77c006d02e64d42f3f2e5008c45a032ffd3a8
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-05-20 18:27:18 +0200

    Add border to HamburgerMenu and fix height and width in general

--- src/Components/HamburgerMenu/HamburgerMenu.tsx
+++ src/Components/HamburgerMenu/HamburgerMenu.tsx
@@ -2,20 +2,25 @@ import { useRef } from "react";
 import "./HamburgerMenu.scss";
 
 function HamburgerMenu() {
-
   const hamburgerMenuRef = useRef<HTMLDivElement>(null);
-  
+
   const ClickHandler = () => {
     hamburgerMenuRef.current?.classList.toggle("active");
     console.log(hamburgerMenuRef);
-  }
+  };
 
   return (
     <>
-      <div className="hamburger-menu" ref={hamburgerMenuRef} onClick={ClickHandler}>
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="hamburger-menu-wrapper">
+        <div
+          className="hamburger-menu"
+          ref={hamburgerMenuRef}
+          onClick={ClickHandler}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </>
   );
--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import "./LandingPage.scss";
 function LandingPage() {
   return (
     <>
+      <h1>Landing Page</h1>
     </>
   );
 }
