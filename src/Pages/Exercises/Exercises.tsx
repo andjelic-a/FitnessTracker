27ce0b122c810733790dc2985f539ee8a9798c6c
Andrej Nenadić <andrejnana445@gmail.com>
2024-06-07 21:36:49 +0200

    Added a context for scroll position and separated exercises component's display into a helper component 'InnerExercises'

--- src/Pages/Exercises/Exercises.tsx
+++ src/Pages/Exercises/Exercises.tsx
@@ -1,36 +1,58 @@
 import "./Exercises.scss";
 import { Await, useLoaderData, useNavigate } from "react-router-dom";
-import { Suspense } from "react";
+import React, { Suspense, useContext, useEffect } from "react";
 import { Immutable, Narrow } from "../../Types/Utility/Models";
 import { FullExercise } from "../../Types/Models/FullExercise";
 import exerciseLoader from "./ExerciseLoader";
+import { testContext } from "../../App";
 
 export default function Exercises() {
   const exercises = useLoaderData() as ReturnType<typeof exerciseLoader>;
-  const navigate = useNavigate();
+  const scrollPositionContext = useContext(testContext);
+
+  useEffect(() => {
+    console.log(scrollPositionContext);
+
+    if (scrollPositionContext > 0.7) {
+    }
+  }, [scrollPositionContext]);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Await resolve={"exercises" in exercises ? exercises.exercises : []}>
         {(
-          exercises: Immutable<Narrow<FullExercise, ["id", "name", "image"]>>[]
-        ) => (
-          <div className="exercises-page-container">
-            {exercises.map((exercise) => {
-              return (
-                <div
-                  className="exercise-card"
-                  key={exercise.id}
-                  onClick={() => navigate(`/exercises/${exercise.id}`)}
-                >
-                  <p>{exercise.name}</p>
-                  <img src={exercise.image} alt="" />
-                </div>
-              );
-            })}
-          </div>
-        )}
+          initialExercises: Immutable<
+            Narrow<FullExercise, ["id", "name", "image"]>
+          >[]
+        ) => {
+          return <InnerExercises exercises={[...initialExercises]} />;
+        }}
       </Await>
     </Suspense>
+  );
+}
+
+function InnerExercises({
+  exercises,
+}: {
+  exercises: Immutable<Narrow<FullExercise, ["id", "name", "image"]>>[];
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="exercises-page-container">
+      {exercises.map((exercise) => {
+        return (
+          <div
+            className="exercise-card"
+            key={exercise.id}
+            onClick={() => navigate(`/exercises/${exercise.id}`)}
+          >
+            <p>{exercise.name}</p>
+            <img src={exercise.image} alt="" />
+          </div>
+        );
+      })}
+    </div>
   );
 }
