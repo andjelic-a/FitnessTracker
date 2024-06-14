db88648293ad8c0267f3b99c5c80ae6d0295fe8e
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-06-14 20:47:23 +0200

    Fix position of profile-user-container in code and add Gear Icon

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import { Suspense } from "react";
 import { Await, useLoaderData, useNavigate } from "react-router-dom";
-import { logout } from "../../Data/User";
+//import { logout } from "../../Data/User";
 import User from "../../Types/Models/User";
 import { Immutable } from "../../Types/Utility/Models";
+import Icon from "../../Components/Icon/Icon";
 import "./Profile.scss";
 
 export default function Profile() {
@@ -12,45 +13,50 @@ export default function Profile() {
   return (
     <div className="profile">
       <div className="profile-container">Profile</div>
-      <div className="profile-user-container">
-        <div className="profile-header">
-          <div className="profile-picture">
-            <img src="" alt="Profile picture" />
-          </div>
-          <div className="profile-user-information">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Await resolve={"user" in userData ? userData.user : null}>
-                {(loadedUserData: Immutable<Omit<User, "id">> | null) => {
-                  if (!loadedUserData) {
-                    navigate("/authentication");
-                    return null;
-                  }
+      <Suspense fallback={<div>Loading...</div>}>
+        <Await resolve={"user" in userData ? userData.user : null}>
+          {(loadedUserData: Immutable<Omit<User, "id">> | null) => {
+            if (!loadedUserData) {
+              navigate("/authentication");
+              return null;
+            }
 
-                  return (
-                    <>
-                      <div className="profile-user-username">
-                        <p>{loadedUserData.name}</p>
+            return (
+              <div className="profile-user-container">
+                <div className="profile-header">
+                  <div className="profile-picture">
+                    <img src="" alt="Profile picture" />
+                  </div>
+                  <div className="profile-user-information">
+                    <div className="profile-user-username">
+                      <p>{loadedUserData.name}</p>
+                      <Icon className="profile-user-settings" id="solid" name="gear" />
+                    </div>
+                    <div className="profile-user-stats">
+                      <div>
+                        <div>Workouts</div>
+                        <div>1</div>
                       </div>
-                      <div className="profile-user-stats">
-                        <div><div>Workouts</div><div>1</div></div>
-                        <div><div>Followers</div><div>1</div></div>
-                        <div><div>Following</div><div>1</div></div>
+                      <div>
+                        <div>Followers</div>
+                        <div>1</div>
                       </div>
-                    </>
-                  );
-                }}
-              </Await>
-            </Suspense>
-          </div>
-        </div>
-        <button className="profile-edit-button">Edit Profile</button>
-        <br /><br /><br /><br /><br />
-        <button
-          onClick={() => logout().then(() => navigate("/authentication"))}
-        >
-          Logout
-        </button>
-      </div>
+                      <div>
+                        <div>Following</div>
+                        <div>1</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button className="profile-edit-button">Edit Profile</button>
+              </div>
+            );
+          }}
+        </Await>
+      </Suspense>
+      {/* <button onClick={() => logout().then(() => navigate("/authentication"))}>
+        Logout
+      </button>*/} 
     </div>
   );
 }
