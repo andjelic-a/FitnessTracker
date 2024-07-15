a9e6702c902657f24ac036b1b6177f714c633ed4
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-07-15 12:48:18 +0200

    Test commit

---
+++ src/Components/WorkoutsContainer/RoutineDisplay/RoutineDisplay.tsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import useOutsideClick from "../../../Hooks/UseOutsideClick";
+import "./RoutineDisplay.scss";
+
+interface RoutineDisplayProps {
+  isVisible: boolean;
+  onClose: () => void;
+  workoutId: string | null;
+}
+
+export default function RoutineDisplay({
+  isVisible,
+  onClose,
+  workoutId,
+}: RoutineDisplayProps) {
+  const routineDisplayRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick(routineDisplayRef, () => {
+    onClose();
+  });
+
+  return (
+    <div
+      ref={routineDisplayRef}
+      className={`routine-display ${isVisible ? "visible" : "hidden"}`}
+    >
+      <div className="routine-display-header">
+        <p className="routine-display-title">Push</p>
+        <button className="routine-display-edit">Edit</button>
+      </div>
+      <div className="routine-display-body">
+      </div>
+      <p>{workoutId}</p>
+    </div>
+  );
+}
