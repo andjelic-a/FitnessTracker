a09f3b35dc8f727b4fb8a10a4394afb82b41c116
Andrej Nenadić <andrejnana445@gmail.com>
2024-07-18 12:09:38 +0200

    Refactor routine display component to use WindowFC

--- src/Components/RoutineDisplay/RoutineDisplay.tsx
+++ src/Components/RoutineDisplay/RoutineDisplay.tsx
@@ -1,15 +1,13 @@
 import "./RoutineDisplay.scss";
 import { useRef, useState } from "react";
-import useOutsideClick from "../../Hooks/UseOutsideClick";
 import Icon from "../Icon/Icon";
 import RoutineDisplayItem from "./RoutineDisplayItem/RoutineDisplayItem";
-import { useNavigate } from "react-router-dom";
-import WindowWrapper from "../WindowWrapper/WindowWrapper";
 import useLoaderData from "../../BetterRouter/UseLoaderData";
 import routineDisplayLoader from "./RoutineDisplayLoader";
 import Async from "../Async/Async";
+import WindowFC from "../WindowWrapper/WindowFC";
 
-export default function RoutineDisplay() {
+const RoutineDisplay = WindowFC(() => {
   const [thumbsUpActive, setThumbsUpActive] = useState<boolean>(false);
   const [commentActive, setCommentActive] = useState<boolean>(false);
   const [favoriteActive, setFavoriteActive] = useState<boolean>(false);
@@ -17,10 +15,6 @@ export default function RoutineDisplay() {
   const routineDisplayRef = useRef<HTMLDivElement>(null);
 
   const data = useLoaderData<typeof routineDisplayLoader>();
-
-  const navigate = useNavigate();
-
-  useOutsideClick(routineDisplayRef, () => navigate("/me"), "left");
 
   const handleThumbsUpClick = () => {
     setThumbsUpActive((prevState) => !prevState);
@@ -35,96 +29,94 @@ export default function RoutineDisplay() {
   };
 
   return (
-    <WindowWrapper>
-      <div ref={routineDisplayRef} className={`routine-display visible`}>
-        <div className="routine-display-header">
-          <Async await={data?.routine}>
-            {(routine) => {
-              if (!routine || routine.code !== "OK") return null;
+    <div ref={routineDisplayRef} className={`routine-display visible`}>
+      <div className="routine-display-header">
+        <Async await={data?.routine}>
+          {(routine) => {
+            if (!routine || routine.code !== "OK") return null;
 
-              return (
-                <>
-                  <p className="routine-display-title">
-                    {routine.content.name}
-                  </p>
-                  <button className="routine-display-edit">Edit</button>
-                </>
-              );
-            }}
-          </Async>
-        </div>
-
-        <div className="routine-display-body">
-          <Async await={data?.routine}>
-            {(routine) => {
-              if (!routine || routine.code !== "OK") return null;
-
-              return (
-                <>
-                  {routine.content.exercises.map((exercise) => (
-                    <RoutineDisplayItem
-                      key={exercise.id}
-                      name={exercise.name}
-                      image={exercise.image}
-                      sets={routine.content.sets.filter(
-                        (set) => set.exerciseId === exercise.id
-                      )}
-                    />
-                  ))}
-                </>
-              );
-            }}
-          </Async>
-        </div>
-
-        <div className="routine-display-footer">
-          <Async await={data?.routine}>
-            {(routine) => {
-              if (!routine || routine.code !== "OK") return null;
-
-              return (
-                <>
-                  {routine.content.description?.trim() !== "" ||
-                    (routine.content.description && (
-                      <div className="routine-display-description-container">
-                        <div className="routine-display-description">
-                          <label className="routine-display-description-placeholder">
-                            Routine Description
-                          </label>
-                          {routine.content.description}
-                        </div>
-                      </div>
-                    ))}
-                  <div className="icon-container">
-                    <Icon
-                      name="thumbs-up"
-                      onClick={handleThumbsUpClick}
-                      className={`routine-display-thumbs-up ${
-                        thumbsUpActive ? "active" : ""
-                      }`}
-                    />
-                    <Icon
-                      onClick={handleCommentClick}
-                      name="comment"
-                      className={`routine-display-comment ${
-                        commentActive ? "active" : ""
-                      }`}
-                    />
-                    <Icon
-                      name="bookmark"
-                      onClick={handleFavoriteClick}
-                      className={`routine-display-bookmark ${
-                        favoriteActive ? "active" : ""
-                      }`}
-                    />
-                  </div>
-                </>
-              );
-            }}
-          </Async>
-        </div>
-        <div className="routine-display-comment-popup">Comments</div>
+            return (
+              <>
+                <p className="routine-display-title">{routine.content.name}</p>
+                <button className="routine-display-edit">Edit</button>
+              </>
+            );
+          }}
+        </Async>
       </div>
-    </WindowWrapper>
+
+      <div className="routine-display-body">
+        <Async await={data?.routine}>
+          {(routine) => {
+            if (!routine || routine.code !== "OK") return null;
+
+            return (
+              <>
+                {routine.content.exercises.map((exercise) => (
+                  <RoutineDisplayItem
+                    key={exercise.id}
+                    name={exercise.name}
+                    image={exercise.image}
+                    sets={routine.content.sets.filter(
+                      (set) => set.exerciseId === exercise.id
+                    )}
+                  />
+                ))}
+              </>
+            );
+          }}
+        </Async>
+      </div>
+
+      <div className="routine-display-footer">
+        <Async await={data?.routine}>
+          {(routine) => {
+            if (!routine || routine.code !== "OK") return null;
+
+            return (
+              <>
+                {routine.content.description?.trim() !== "" ||
+                  (routine.content.description && (
+                    <div className="routine-display-description-container">
+                      <div className="routine-display-description">
+                        <label className="routine-display-description-placeholder">
+                          Routine Description
+                        </label>
+                        {routine.content.description}
+                      </div>
+                    </div>
+                  ))}
+                <div className="icon-container">
+                  <Icon
+                    name="thumbs-up"
+                    onClick={handleThumbsUpClick}
+                    className={`routine-display-thumbs-up ${
+                      thumbsUpActive ? "active" : ""
+                    }`}
+                  />
+                  <Icon
+                    onClick={handleCommentClick}
+                    name="comment"
+                    className={`routine-display-comment ${
+                      commentActive ? "active" : ""
+                    }`}
+                  />
+                  <Icon
+                    name="bookmark"
+                    onClick={handleFavoriteClick}
+                    className={`routine-display-bookmark ${
+                      favoriteActive ? "active" : ""
+                    }`}
+                  />
+                </div>
+              </>
+            );
+          }}
+        </Async>
+      </div>
+      <div className="routine-display-comment-popup">Comments</div>
+    </div>
   );
-}
+});
+
+export default RoutineDisplay;
