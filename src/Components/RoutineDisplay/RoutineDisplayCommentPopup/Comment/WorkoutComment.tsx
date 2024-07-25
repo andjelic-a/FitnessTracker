fd63813e987f289b85656b1d7c3171cd483bc595
Andrej Nenadić <andrejnana445@gmail.com>
2024-07-25 17:47:29 +0200

    Greatly improve performance and fix all flickering bugs in comment section by memoizing all comment elements (including replies) and moving reply-reading logic back to comment component

--- src/Components/RoutineDisplay/RoutineDisplayCommentPopup/Comment/WorkoutComment.tsx
+++ src/Components/RoutineDisplay/RoutineDisplayCommentPopup/Comment/WorkoutComment.tsx
@@ -1,11 +1,12 @@
 import "./WorkoutComment.scss";
 import { Schema } from "../../../../Types/Endpoints/SchemaParser";
 import Icon from "../../../Icon/Icon";
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import sendAPIRequest from "../../../../Data/SendAPIRequest";
 import formatDateSince from "../../../../Utility/FormatDateSince";
-import Async from "../../../Async/Async";
 import CommentInputField from "../CommentInputField/CommentInputField";
+import { v4 } from "uuid";
+import { getProfileCache } from "../../../../Pages/Profile/ProfileCache";
 
 type WorkoutCommentProps = {
   comment: Schema<"SimpleWorkoutCommentResponseDTO">;
@@ -13,210 +14,297 @@ type WorkoutCommentProps = {
 
 type ParentProps = {
   isReply?: false;
-  replies: Promise<{
-    replies: Schema<"SimpleWorkoutCommentResponseDTO">[];
-    reachedEnd: boolean;
-    newRepliesCount: number;
-  }> | null;
-  index: number;
-  requestReplies: (i: number) => void;
-  requestMoreReplies: (i: number) => Promise<void>;
-  onReply: (
-    i: number,
-    newReply: Schema<"CreateWorkoutCommentRequestDTO">
-  ) => void;
+  workoutId: string;
 };
 
 type ReplyProps = {
   isReply: true;
   parentId: string;
-  onReply: (newReply: Schema<"CreateWorkoutCommentRequestDTO">) => void;
+  onReplyToChild: (newReply: Schema<"CreateWorkoutCommentRequestDTO">) => void;
 };
 
-export default function WorkoutComment({
-  comment,
-  ...props
-}: WorkoutCommentProps) {
-  const isWaitingForResponse = useRef<boolean>(false);
+const WorkoutComment = React.memo<WorkoutCommentProps>(
+  ({ comment, ...props }) => {
+    const isWaitingForResponse = useRef<boolean>(false);
 
-  const [isLiked, setIsLiked] = useState<boolean | null>(null);
-  const [likeCount, setLikeCount] = useState<number>(0);
+    const [isLiked, setIsLiked] = useState<boolean | null>(null);
+    const [likeCount, setLikeCount] = useState<number>(0);
 
-  const [isReplying, setIsReplying] = useState<boolean>(false);
-  const [replyCount, setReplyCount] = useState<number>(0);
-  const [repliesExpanded, setRepliesExpanded] = useState<boolean>(false);
+    const [isReplying, setIsReplying] = useState<boolean>(false);
+    const [replyCount, setReplyCount] = useState<number>(0);
+    const [repliesExpanded, setRepliesExpanded] = useState<boolean>(false);
 
-  useEffect(() => {
-    setIsLiked(comment.isLiked);
-    setLikeCount(comment.likeCount);
-    setReplyCount(comment.replyCount);
-  }, [comment]);
+    const [replies, setReplies] = useState<{
+      replies: Schema<"SimpleWorkoutCommentResponseDTO">[];
+      newRepliesCount: number;
+      reachedEnd: boolean;
+    } | null>(null);
 
-  function handleLikeClick() {
-    if (isWaitingForResponse.current) return;
-    isWaitingForResponse.current = true;
+    async function requestReplies() {
+      if (props.isReply) return;
 
-    sendAPIRequest("/api/workout/comment/{id}/like", {
-      method: !isLiked ? "post" : "delete",
-      parameters: {
-        id: comment.id,
-      },
-    }).then(() => void (isWaitingForResponse.current = false));
-
-    setLikeCount((prevState) => prevState + (isLiked ? -1 : 1));
-    setIsLiked((prevState) => !prevState);
-  }
-
-  function handleRepliesClick() {
-    setRepliesExpanded((prevState) => !prevState);
-  }
-
-  function handleReplyBtnClick() {
-    setIsReplying(true);
-  }
-
-  function handleCreateReply(
-    newComment: Schema<"CreateWorkoutCommentRequestDTO">
-  ) {
-    sendAPIRequest("/api/workout/{workoutId}/comment/{commentId}/reply", {
-      method: "post",
-      parameters: {
-        workoutId: comment.workoutId,
-        commentId: props.isReply ? props.parentId : comment.id,
-      },
-      payload: newComment,
-    });
-
-    setIsReplying(false);
-    if (!props.isReply) {
-      setReplyCount((prevState) => prevState + 1);
-      props.onReply(props.index, newComment);
-    } else props.onReply(newComment);
-  }
-
-  const getReplies = useCallback(async () => {
-    if (!props.isReply && props.replies) {
-      const reply = await props.replies;
+      const awaitedReplies = replies;
 
       if (
-        reply.replies.length !== 0 &&
-        reply.replies.length !== reply.newRepliesCount
+        awaitedReplies &&
+        awaitedReplies.replies.length !== 0 &&
+        awaitedReplies.replies.length !== awaitedReplies.newRepliesCount
       )
-        return props.replies;
+        return;
+
+      const data = await sendAPIRequest(
+        "/api/workout/{workoutId}/comment/{commentId}/reply",
+        {
+          method: "get",
+          parameters: {
+            commentId: comment.id,
+            workoutId: props.workoutId,
+            limit: 10,
+            offset: 0,
+          },
+        }
+      );
+
+      if (data.code !== "OK") return;
+
+      setReplies((prev) =>
+        prev
+          ? {
+              replies: [...prev.replies, ...data.content],
+              reachedEnd: data.content.length < 10,
+              newRepliesCount: prev.newRepliesCount,
+            }
+          : {
+              replies: data.content,
+              reachedEnd: data.content.length < 10,
+              newRepliesCount: 0,
+            }
+      );
     }
 
-    return Promise.resolve({
-      replies: [],
-      reachedEnd: false,
-      newRepliesCount: 0,
-    });
-  }, [props]);
+    async function requestMoreReplies() {
+      if (props.isReply || !replies) return;
 
-  useEffect(() => {
-    if (!repliesExpanded || props.isReply) return;
+      const awaitReplies = replies;
+      if (!awaitReplies || awaitReplies.reachedEnd) return;
 
-    props.requestReplies(props.index);
-  }, [repliesExpanded]);
+      const data = await sendAPIRequest(
+        "/api/workout/{workoutId}/comment/{commentId}/reply",
+        {
+          method: "get",
+          parameters: {
+            commentId: comment.id,
+            workoutId: props.workoutId,
+            limit: 10,
+            offset: awaitReplies.replies.length - awaitReplies.newRepliesCount,
+          },
+        }
+      );
 
-  function handleShowMoreRepliesClick() {
-    if (props.isReply || isWaitingForResponse.current) return;
+      if (data.code !== "OK") return;
 
-    isWaitingForResponse.current = true;
+      setReplies((prev) => {
+        prev = {
+          replies: [...awaitReplies.replies, ...data.content],
+          reachedEnd: data.content.length < 10,
+          newRepliesCount: awaitReplies.newRepliesCount,
+        };
+        return prev;
+      });
+    }
 
-    props
-      .requestMoreReplies(props.index)
-      .then(() => void (isWaitingForResponse.current = false));
-  }
+    useEffect(() => {
+      setIsLiked(comment.isLiked);
+      setLikeCount(comment.likeCount);
+      setReplyCount(comment.replyCount);
+    }, [comment]);
 
-  return (
-    <div className="workout-comment-container">
-      <div className="image-container">
-        <img
-          src={comment.creator.image ?? "/DefaultProfilePicture.png"}
-          alt={`Profile picture of a user named ${comment.creator.name}`}
-        />
-      </div>
+    function handleLikeClick() {
+      if (isWaitingForResponse.current) return;
+      isWaitingForResponse.current = true;
 
-      <div className="main">
-        <div className="header">
-          <p className="name">@{comment.creator.name}</p>
-          <p className="dot">●</p>
-          <p className="date">{formatDateSince(new Date(comment.createdAt))}</p>
-        </div>
+      sendAPIRequest("/api/workout/comment/{id}/like", {
+        method: !isLiked ? "post" : "delete",
+        parameters: {
+          id: comment.id,
+        },
+      }).then(() => void (isWaitingForResponse.current = false));
 
-        <div className="body">
-          <p>{comment.text}</p>
-        </div>
+      setLikeCount((prevState) => prevState + (isLiked ? -1 : 1));
+      setIsLiked((prevState) => !prevState);
+    }
 
-        <div className="footer">
-          <div className="footer-interaction-container">
-            <div className="like-container">
-              <Icon
-                name="thumbs-up"
-                onClick={handleLikeClick}
-                className={`like-btn ${isLiked ? "active" : ""}`}
-              />
-              {likeCount > 0 && <p>{likeCount}</p>}
+    function handleRepliesClick() {
+      setRepliesExpanded((prevState) => !prevState);
+    }
+
+    function handleReplyBtnClick() {
+      setIsReplying(true);
+    }
+
+    function handleCreateReply(
+      newReply: Schema<"CreateWorkoutCommentRequestDTO">
+    ) {
+      sendAPIRequest("/api/workout/{workoutId}/comment/{commentId}/reply", {
+        method: "post",
+        parameters: {
+          workoutId: comment.workoutId,
+          commentId: props.isReply ? props.parentId : comment.id,
+        },
+        payload: newReply,
+      });
+
+      if (!props.isReply) handleNewReplyUIUpdate(newReply);
+      else props.onReplyToChild(newReply);
+      setIsReplying(false);
+    }
+
+    async function handleNewReplyUIUpdate(
+      newReply: Schema<"CreateWorkoutCommentRequestDTO">
+    ) {
+      const userData = getProfileCache();
+      if (props.isReply || !userData) return;
+
+      const user = await userData.user;
+      if (user.code !== "OK") return;
+
+      const newCommentSimulatedResponse: Schema<"SimpleWorkoutCommentResponseDTO"> =
+        {
+          id: v4(),
+          createdAt: new Date().toISOString(),
+          creator: user.content,
+          isCreator: true,
+          isLiked: false,
+          likeCount: 0,
+          replyCount: 0,
+          text: newReply.comment,
+          workoutId: props.workoutId,
+        };
+
+      setReplies((prev) =>
+        prev
+          ? {
+              replies: [...prev.replies, newCommentSimulatedResponse],
+              reachedEnd: prev.reachedEnd,
+              newRepliesCount: prev.newRepliesCount + 1,
+            }
+          : {
+              replies: [newCommentSimulatedResponse],
+              reachedEnd: false,
+              newRepliesCount: 1,
+            }
+      );
+      setReplyCount((prevState) => prevState + 1);
+    }
+
+    useEffect(() => {
+      if (!repliesExpanded || props.isReply) return;
+
+      requestReplies();
+    }, [repliesExpanded]);
+
+    function handleShowMoreRepliesClick() {
+      if (props.isReply || isWaitingForResponse.current) return;
+
+      isWaitingForResponse.current = true;
+
+      requestMoreReplies().then(
+        () => void (isWaitingForResponse.current = false)
+      );
+    }
+
+    const replyElements = useMemo(() => {
+      if (!replies) return null;
+
+      return (
+        <div className="comment-replies-container">
+          {replies.replies.map((reply) => (
+            <WorkoutComment
+              key={reply.id}
+              isReply
+              parentId={comment.id}
+              comment={reply}
+              onReplyToChild={(newReply) =>
+                void (props.isReply
+                  ? props.onReplyToChild(newReply)
+                  : handleNewReplyUIUpdate(newReply))
+              }
+            />
+          ))}
+
+          {!replies.reachedEnd && (
+            <div className="show-more" onClick={handleShowMoreRepliesClick}>
+              <Icon name="long-arrow-down" />
+              <p>Show more replies</p>
             </div>
-            <button className="reply-btn" onClick={handleReplyBtnClick}>
-              Reply
-            </button>
+          )}
+        </div>
+      );
+    }, [replies]);
+
+    return (
+      <div className="workout-comment-container">
+        <div className="image-container">
+          <img
+            src={comment.creator.image ?? "/DefaultProfilePicture.png"}
+            alt={`Profile picture of a user named ${comment.creator.name}`}
+          />
+        </div>
+
+        <div className="main">
+          <div className="header">
+            <p className="name">@{comment.creator.name}</p>
+            <p className="dot">●</p>
+            <p className="date">
+              {formatDateSince(new Date(comment.createdAt))}
+            </p>
           </div>
 
-          {isReplying && (
-            <CommentInputField
-              type="reply"
-              onSubmit={handleCreateReply}
-              onCancel={() => setIsReplying(false)}
-            />
-          )}
+          <div className="body">
+            <p>{comment.text}</p>
+          </div>
 
-          {!props.isReply && replyCount > 0 && (
-            <>
-              <div
-                className="reply-count-container"
-                onClick={handleRepliesClick}
-              >
-                <Icon name={`caret-${repliesExpanded ? "up" : "down"}`} />
-                <p>{replyCount} replies</p>
+          <div className="footer">
+            <div className="footer-interaction-container">
+              <div className="like-container">
+                <Icon
+                  name="thumbs-up"
+                  onClick={handleLikeClick}
+                  className={`like-btn ${isLiked ? "active" : ""}`}
+                />
+                {likeCount > 0 && <p>{likeCount}</p>}
               </div>
+              <button className="reply-btn" onClick={handleReplyBtnClick}>
+                Reply
+              </button>
+            </div>
 
-              {repliesExpanded && (
-                <Async await={getReplies()}>
-                  {(replies) => (
-                    <div className="comment-replies-container">
-                      {replies.replies.map((reply) => (
-                        <WorkoutComment
-                          key={reply.id}
-                          isReply
-                          parentId={comment.id}
-                          onReply={(newReply) => {
-                            if (props.isReply) return;
+            {isReplying && (
+              <CommentInputField
+                type="reply"
+                onSubmit={handleCreateReply}
+                onCancel={() => setIsReplying(false)}
+              />
+            )}
 
-                            setReplyCount((prevState) => prevState + 1);
-                            props.onReply(props.index, newReply);
-                          }}
-                          comment={reply}
-                        />
-                      ))}
+            {!props.isReply && replyCount > 0 && (
+              <>
+                <div
+                  className="reply-count-container"
+                  onClick={handleRepliesClick}
+                >
+                  <Icon name={`caret-${repliesExpanded ? "up" : "down"}`} />
+                  <p>{replyCount} replies</p>
+                </div>
 
-                      {!replies.reachedEnd && (
-                        <div
-                          className="show-more"
-                          onClick={handleShowMoreRepliesClick}
-                        >
-                          <Icon name="long-arrow-down" />
-                          <p>Show more replies</p>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </Async>
-              )}
-            </>
-          )}
+                {repliesExpanded && <>{replyElements}</>}
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default WorkoutComment;
--- src/Components/RoutineDisplay/RoutineDisplayCommentPopup/WorkoutCommentSection.tsx
+++ src/Components/RoutineDisplay/RoutineDisplayCommentPopup/WorkoutCommentSection.tsx
@@ -1,14 +1,12 @@
 import "./WorkoutCommentSection.scss";
 import { Schema } from "../../../Types/Endpoints/SchemaParser";
-import { useEffect, useRef, useState } from "react";
+import { useMemo, useRef } from "react";
 import useOutsideClick from "../../../Hooks/UseOutsideClick";
 import sendAPIRequest from "../../../Data/SendAPIRequest";
 import WorkoutComment from "./Comment/WorkoutComment";
 import CommentInputField from "./CommentInputField/CommentInputField";
 import useScrollTrigger from "../../../Hooks/UseScrollTrigger";
-import { motion as Motion } from "framer-motion";
-import { getProfileCache } from "../../../Pages/Profile/ProfileCache";
-import { v4 } from "uuid";
+import { AnimatePresence, motion as Motion } from "framer-motion";
 
 type WorkoutCommentSectionProps = {
   workoutId: string;
@@ -33,18 +31,6 @@ export default function WorkoutCommentSection({
   useScrollTrigger(scrollableWrapperRef, 0.7, onRequireLazyLoad);
   useOutsideClick(wrapperRef, onRequireClose);
 
-  useEffect(() => {
-    setReplies((prev) => {
-      for (let i = 0; i < comments.length; i++) {
-        if (prev[i]) continue;
-
-        prev[i] = null;
-      }
-
-      return prev.slice();
-    });
-  }, [comments]);
-
   function handleCreateComment(
     newComment: Schema<"CreateWorkoutCommentRequestDTO">
   ) {
@@ -59,143 +45,9 @@ export default function WorkoutCommentSection({
     onAddNewComment(newComment);
   }
 
-  const [replies, setReplies] = useState<
-    (Promise<{
-      replies: Schema<"SimpleWorkoutCommentResponseDTO">[];
-      newRepliesCount: number;
-      reachedEnd: boolean;
-    }> | null)[]
-  >([]);
-
-  async function getInitialReplies(i: number) {
-    console.log("0");
-    if (replies[i]) {
-      console.log("1");
-      const reply = await replies[i];
-      if (
-        reply.replies.length !== 0 &&
-        reply.replies.length !== reply.newRepliesCount
-      )
-        return reply;
-
-      console.log("2");
-    }
-
-    const data = sendAPIRequest(
-      "/api/workout/{workoutId}/comment/{commentId}/reply",
-      {
-        method: "get",
-        parameters: {
-          commentId: comments[i].id,
-          workoutId: workoutId,
-          limit: 10,
-          offset: 0,
-        },
-      }
-    ).then((data) => {
-      if (data.code !== "OK") return [];
-
-      // currentRepliesPromiseRef.current = null;
-      return data.content;
-    });
-
-    setReplies((prev) => {
-      prev[i] = Promise.all([data, prev[i]]).then(([data, prev]) => {
-        if (prev)
-          return {
-            replies: [...prev.replies, ...data],
-            reachedEnd: data.length < 10,
-            newRepliesCount: prev.newRepliesCount,
-          };
-
-        return {
-          replies: data,
-          reachedEnd: data.length < 10,
-          newRepliesCount: 0,
-        };
-      });
-      return prev.slice();
-    });
-  }
-
-  async function getMoreReplies(i: number) {
-    if (!replies[i]) return;
-
-    const reply = await replies[i];
-    if (!reply || reply.reachedEnd) return;
-
-    const data = sendAPIRequest(
-      "/api/workout/{workoutId}/comment/{commentId}/reply",
-      {
-        method: "get",
-        parameters: {
-          commentId: comments[i].id,
-          workoutId: workoutId,
-          limit: 10,
-          offset: reply.replies.length - reply.newRepliesCount,
-        },
-      }
-    ).then((data) => {
-      if (data.code !== "OK") return [];
-
-      // currentRepliesPromiseRef.current = null;
-      return data.content;
-    });
-
-    setReplies((prev) => {
-      prev[i] = data.then((x) => ({
-        replies: [...reply.replies, ...x],
-        reachedEnd: x.length < 10,
-        newRepliesCount: reply.newRepliesCount, //WIP
-      }));
-      return prev.slice();
-    });
-  }
-
-  function handleNewReply(
-    i: number,
-    newReply: Schema<"CreateWorkoutCommentRequestDTO">
-  ) {
-    const userData = getProfileCache();
-    if (!userData) return;
-
-    userData.user.then((user) => {
-      if (user.code !== "OK") return;
-
-      const newCommentSimulatedResponse: Schema<"SimpleWorkoutCommentResponseDTO"> =
-        {
-          id: v4(),
-          createdAt: new Date().toISOString(),
-          creator: user.content,
-          isCreator: true,
-          isLiked: false,
-          likeCount: 0,
-          replyCount: 0,
-          text: newReply.comment,
-          workoutId: workoutId,
-        };
-
-      setReplies((prev) => {
-        prev[i] = prev[i]
-          ? prev[i].then((x) => ({
-              replies: [...x.replies, newCommentSimulatedResponse],
-              reachedEnd: x.reachedEnd,
-              newRepliesCount: x.newRepliesCount + 1,
-            }))
-          : Promise.resolve({
-              replies: [newCommentSimulatedResponse],
-              reachedEnd: false,
-              newRepliesCount: 1,
-            });
-
-        return prev.slice();
-      });
-    });
-  }
-
-  return (
-    <Motion.div
-      variants={{
+  const motionProps = useMemo(
+    () => ({
+      variants: {
         hidden: {
           opacity: 0,
           y: 300,
@@ -208,14 +60,36 @@ export default function WorkoutCommentSection({
           opacity: 0,
           y: 300,
         },
-      }}
-      initial="hidden"
-      animate="enter"
-      exit="exit"
-      transition={{
+      },
+      initial: "hidden",
+      animate: "enter",
+      exit: "exit",
+      transition: {
         duration: 0.3,
         type: "just",
-      }}
+      },
+    }),
+    []
+  );
+
+  const commentELements = useMemo(
+    () => (
+      <>
+        {comments.map((comment) => (
+          <WorkoutComment
+            key={comment.id}
+            comment={comment}
+            workoutId={workoutId}
+          />
+        ))}
+      </>
+    ),
+    [comments]
+  );
+
+  return (
+    <Motion.div
+      {...motionProps}
       className="workout-display-comment-section"
       ref={wrapperRef}
     >
@@ -227,17 +101,7 @@ export default function WorkoutCommentSection({
         <CommentInputField type="comment" onSubmit={handleCreateComment} />
 
         <div className="workout-comments-body">
-          {comments.map((comment, i) => (
-            <WorkoutComment
-              key={comment.id}
-              comment={comment}
-              replies={replies[i]}
-              requestReplies={getInitialReplies}
-              requestMoreReplies={getMoreReplies}
-              index={i}
-              onReply={handleNewReply}
-            />
-          ))}
+          <AnimatePresence>{commentELements}</AnimatePresence>
         </div>
       </div>
     </Motion.div>
