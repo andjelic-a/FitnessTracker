5c9852282396f06255d5c18a66cc5f0e0f0c68b5
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-07-30 17:00:52 +0200

    Add: Image container, muscle groups and description to Exercise component

--- src/Components/Exercise/Exercise.tsx
+++ src/Components/Exercise/Exercise.tsx
@@ -17,6 +17,30 @@ export default function Exercise({ setIsOpen, isOpen }: ExerciseProps) {
           name="xmark"
         />
       </div>
+      <div className="exercise-body">
+        <div className="exercise-body-image-container">
+          <img src="../../public/DefaultProfilePicture.png" />
+        </div>
+        <div className="exercise-body-muscles">
+          <div>
+            <h3>Primary Muscles:</h3>
+            <p>Triceps, Front shoulder, Upper chest</p>
+          </div>
+          <div>
+            <h3>Secondary Muscles:</h3>
+            <p>Triceps, Front shoulder, Upper chest</p>
+          </div>
+        </div>
+        <div className="exercise-body-description">
+          <h3>Description:</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit quae labore ex, blanditiis aut ut porro ullam natus
+            minima assumenda accusantium numquam ab, voluptate suscipit soluta
+            mollitia quam maxime obcaecati.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
