d0a17f7308dfb4c9b527eb166ff8d623aad70e4b
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-07-31 14:48:45 +0200

    Add: Changes to settings page and implement cancel functionality

--- src/Pages/Settings/Settings.tsx
+++ src/Pages/Settings/Settings.tsx
@@ -1,8 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import WindowFC from "../../Components/WindowWrapper/WindowFC";
 import "./Settings.scss";
 
 const Settings = WindowFC(({}, wrapperRef) => {
-  return <div ref={wrapperRef}>Settings</div>;
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div ref={wrapperRef}>
+      <div className="settings">
+        <div className="settings-item">Edit profile</div>
+        <div className="settings-item">Authentication</div>
+        <div className="settings-item">Privacy</div>
+        <div className="settings-item">Log out</div>
+        <div className="settings-item" onClick={handleCancel}>Cancel</div>
+      </div>
+    </div>
+  );
 });
 
 export default Settings;
