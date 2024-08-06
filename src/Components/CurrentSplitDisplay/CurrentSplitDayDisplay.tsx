2a86967d4f432cbd4e0da5c5942a4c2ed5489d4e
Andrej Nenadić <andrejnana445@gmail.com>
2024-08-06 23:38:57 +0200

    Add styling for todays workout (animated border using before and after pseudo elements)

--- src/Components/CurrentSplitDisplay/CurrentSplitDayDisplay.tsx
+++ src/Components/CurrentSplitDisplay/CurrentSplitDayDisplay.tsx
@@ -27,7 +27,7 @@ export default function CurrentSplitDayDisplay({
       ) : (
         <WorkoutPreviewDisplay
           workout={props.workout}
-          className={"workout " + "skipped"}
+          className={"workout " + props.status}
         />
       )}
     </>
