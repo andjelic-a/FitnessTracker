e7d96f7239449b814c410b097b757b35849b22ad
Andrej Nenadić <andrejnana445@gmail.com>
2024-08-10 12:23:52 +0200

    Animate active-indicator using gsap's Flip and react-reverse-portal library

--- src/Components/ProfileWorkoutTabs/ProfileWorkoutTabs.tsx
+++ src/Components/ProfileWorkoutTabs/ProfileWorkoutTabs.tsx
@@ -1,37 +1,65 @@
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Schema } from "../../Types/Endpoints/SchemaParser";
 import Async from "../Async/Async";
 import WorkoutCarousel from "../WorkoutCarousel/WorkoutCarousel";
 import WorkoutPreview from "../WorkoutPreview/WorkoutPreview";
 import "./ProfileWorkoutTabs.scss";
+import * as portals from "react-reverse-portal";
+
+import gsap from "gsap";
+import Flip from "gsap/dist/Flip";
+gsap.registerPlugin(Flip);
 
 type ProfileWorkoutTabsProps = {
   initialCreatedWorkouts: Promise<Schema<"SimpleWorkoutResponseDTO">[]>;
 };
 
-type Tabs = "created" | "favorite" | "liked";
+type Tab = "created" | "favorite" | "liked";
 
 export default function ProfileWorkoutTabs({
   initialCreatedWorkouts,
 }: ProfileWorkoutTabsProps) {
-  const [openTab, setOpenTab] = useState<Tabs>("created");
+  const [openTab, setOpenTab] = useState<Tab>("created");
+  const stateRef = useRef<Flip.FlipState | null>(null);
+  const portalNode = useMemo(() => portals.createHtmlPortalNode(), []);
+
+  useEffect(() => {
+    if (!stateRef.current) return;
+
+    Flip.from(stateRef.current, {
+      scale: true,
+      duration: 0.25,
+      ease: "sine.inOut",
+    });
+  }, [openTab]);
+
+  function handleOpenTab(tab: Tab) {
+    stateRef.current = Flip.getState(".active-indicator");
+    setOpenTab(tab);
+  }
 
   return (
     <div className="profile-workout-tabs-container">
+      <portals.InPortal node={portalNode}>
+        <div className="active-indicator" data-flip-id="1" />
+      </portals.InPortal>
+
       <div className="tabs-header">
         <div className="tab">
-          <button onClick={() => setOpenTab("created")}>Created</button>
-          {openTab === "created" && <div className="active-indicator" />}
+          <button onClick={() => handleOpenTab("created")}>Created</button>
+          {openTab === "created" && <portals.OutPortal node={portalNode} />}
         </div>
 
         <div className="tab">
-          <button onClick={() => setOpenTab("favorite")}>Favorite</button>
-          {openTab === "favorite" && <div className="active-indicator" />}
+          <button onClick={() => handleOpenTab("favorite")}>Favorite</button>
+
+          {openTab === "favorite" && <portals.OutPortal node={portalNode} />}
         </div>
 
         <div className="tab">
-          <button onClick={() => setOpenTab("liked")}>Liked</button>
-          {openTab === "liked" && <div className="active-indicator" />}
+          <button onClick={() => handleOpenTab("liked")}>Liked</button>
+
+          {openTab === "liked" && <portals.OutPortal node={portalNode} />}
         </div>
       </div>
 
