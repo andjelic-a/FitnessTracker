ace6e1c79a048e26fd782dc3782c5320688d860c
Andrej Nenadić <andrejnana445@gmail.com>
2024-08-22 22:55:43 +0200

    Add props for adding a modal popup dialog when request to close a window (WindowFC). Implement a closing confirmation dialog in WorkoutCreator

--- src/Components/WorkoutCreator/WorkoutCreator.tsx
+++ src/Components/WorkoutCreator/WorkoutCreator.tsx
@@ -1,5 +1,5 @@
 import "./WorkoutCreator.scss";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { WorkoutItemData } from "../WorkoutSetCreator/WorkoutItem/WorkoutItem";
 import Icon from "../Icon/Icon";
 import sendAPIRequest from "../../Data/SendAPIRequest";
@@ -11,202 +11,244 @@ import { getProfileCache } from "../../Pages/Profile/ProfileCache";
 import { Tooltip } from "react-tooltip";
 import CurrentEditingWorkoutSetsContext from "../../Contexts/CurrentEditingWorkoutSetsContext";
 
-const WorkoutCreator = WindowFC(({}, _, onClose) => {
-  // console.log("Rerendering create workout window");
+const WorkoutCreator = WindowFC(
+  ({}, wrapperRef, onClose, setModalConfirmationOpeningCondition) => {
+    const newWorkoutsContext = useContext(NewWorkoutsContext);
 
-  const newWorkoutsContext = useContext(NewWorkoutsContext);
-  const wrapperRef = useRef<HTMLDivElement>(null);
+    const [isPublic, setIsPublic] = useState<boolean>(false);
+    const [currentSets, setCurrentSets] = useState<WorkoutItemData[]>([]);
 
-  const [isPublic, setIsPublic] = useState<boolean>(false);
-  const [currentSets, setCurrentSets] = useState<WorkoutItemData[]>([]);
+    const workoutTitleRef = useRef<HTMLInputElement | null>(null);
+    const descriptionTextAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  const workoutTitleRef = useRef<HTMLInputElement | null>(null);
-  const descriptionTextAreaRef = useRef<HTMLTextAreaElement>(null);
+    const validateTitle = (): boolean => {
+      if (!workoutTitleRef.current?.value) {
+        workoutTitleRef.current?.classList.add("invalid");
+        return false;
+      }
 
-  const validateTitle = (): boolean => {
-    if (!workoutTitleRef.current?.value) {
-      workoutTitleRef.current?.classList.add("invalid");
-      return false;
-    }
-
-    workoutTitleRef.current?.classList.remove("invalid");
-    return true;
-  };
-
-  const validateSets = (): boolean => {
-    if (currentSets.length <= 0) {
-      wrapperRef.current?.classList.add("invalid-exercise-selection");
-      return false;
-    }
-
-    wrapperRef.current?.classList.remove("invalid-exercise-selection");
-    return true;
-  };
-
-  const handleSaveClick = async () => {
-    const userResponse = await getProfileCache()?.user;
-    const user = userResponse?.code !== "OK" ? null : userResponse?.content;
-
-    let isValid = validateTitle();
-    isValid = validateSets() && isValid;
-
-    if (
-      !user ||
-      !descriptionTextAreaRef.current ||
-      !workoutTitleRef.current ||
-      !isValid
-    )
-      return;
-
-    const newWorkout: Schema<"CreateWorkoutRequestDTO"> = {
-      isPublic: isPublic,
-      name: workoutTitleRef.current.value,
-      description: descriptionTextAreaRef.current.value,
-      sets: currentSets
-        .flatMap((workoutItem) =>
-          workoutItem.sets.map((set) => ({
-            exerciseId: workoutItem.exercise.id,
-            set: set,
-          }))
-        )
-        .map((x) => {
-          let repRange = x.set.repRange
-            .trim()
-            .split("-")
-            .map((x) => parseInt(x));
-
-          if (repRange.length === 1)
-            repRange = x.set.repRange
-              .trim()
-              .split(" ")
-              .map((x) => parseInt(x));
-
-          if (repRange.length === 1) repRange = [repRange[0], repRange[0]];
-
-          const enumValue = ["1", "w", "d", "f"].indexOf(x.set.type ?? "1");
-
-          const rir =
-            !x.set.type || x.set.type === "1"
-              ? x.set.rir
-              : x.set.type === "w"
-              ? -1
-              : 0;
-
-          return {
-            exerciseId: x.exerciseId,
-            bottomRepRange: repRange[0],
-            topRepRange: repRange[1],
-            riR: rir,
-            type: enumValue as Schema<"SetType">,
-          };
-        }),
+      workoutTitleRef.current?.classList.remove("invalid");
+      return true;
     };
 
-    descriptionTextAreaRef.current.value = "";
-    descriptionTextAreaRef.current.blur();
-    sendAPIRequest("/api/workout", {
-      method: "post",
-      payload: newWorkout,
-    }).then((newWorkout) => {
-      if (newWorkout.code !== "Created") return;
+    const validateSets = (): boolean => {
+      if (currentSets.length <= 0) {
+        wrapperRef.current?.classList.add("invalid-exercise-selection");
+        return false;
+      }
 
-      const simulatedResponse: Schema<"SimpleWorkoutResponseDTO"> = {
-        id: newWorkout.content.id,
-        name: newWorkout.content.name,
-        isPublic: newWorkout.content.isPublic,
-        creator: {
-          id: user.id,
-          name: user.name,
-          image: user.image,
-        },
-        description: "",
+      wrapperRef.current?.classList.remove("invalid-exercise-selection");
+      return true;
+    };
+
+    const handleSaveClick = async () => {
+      const userResponse = await getProfileCache()?.user;
+      const user = userResponse?.code !== "OK" ? null : userResponse?.content;
+
+      let isValid = validateTitle();
+      isValid = validateSets() && isValid;
+
+      if (
+        !user ||
+        !descriptionTextAreaRef.current ||
+        !workoutTitleRef.current ||
+        !isValid
+      )
+        return;
+
+      const newWorkout: Schema<"CreateWorkoutRequestDTO"> = {
+        isPublic: isPublic,
+        name: workoutTitleRef.current.value,
+        description: descriptionTextAreaRef.current.value,
+        sets: currentSets
+          .flatMap((workoutItem) =>
+            workoutItem.sets.map((set) => ({
+              exerciseId: workoutItem.exercise.id,
+              set: set,
+            }))
+          )
+          .map((x) => {
+            let repRange = x.set.repRange
+              .trim()
+              .split("-")
+              .map((x) => parseInt(x));
+
+            if (repRange.length === 1)
+              repRange = x.set.repRange
+                .trim()
+                .split(" ")
+                .map((x) => parseInt(x));
+
+            if (repRange.length === 1) repRange = [repRange[0], repRange[0]];
+
+            const enumValue = ["1", "w", "d", "f"].indexOf(x.set.type ?? "1");
+
+            const rir =
+              !x.set.type || x.set.type === "1"
+                ? x.set.rir
+                : x.set.type === "w"
+                ? -1
+                : 0;
+
+            return {
+              exerciseId: x.exerciseId,
+              bottomRepRange: repRange[0],
+              topRepRange: repRange[1],
+              riR: rir,
+              type: enumValue as Schema<"SetType">,
+            };
+          }),
       };
 
-      onClose();
-      newWorkoutsContext.addWorkout(simulatedResponse);
-    });
-  };
+      descriptionTextAreaRef.current.value = "";
+      descriptionTextAreaRef.current.blur();
+      sendAPIRequest("/api/workout", {
+        method: "post",
+        payload: newWorkout,
+      }).then((newWorkout) => {
+        if (newWorkout.code !== "Created") return;
 
-  return (
-    <CurrentEditingWorkoutSetsContext.Provider
-      value={{
-        currentSets,
-        setCurrentSets,
-      }}
-    >
-      <div ref={wrapperRef} className="create-workout-window">
-        <div className="header">
-          <input
-            ref={workoutTitleRef}
-            type="text"
-            className="title"
-            placeholder="Workout title"
-            maxLength={25}
-          />
+        const simulatedResponse: Schema<"SimpleWorkoutResponseDTO"> = {
+          id: newWorkout.content.id,
+          name: newWorkout.content.name,
+          isPublic: newWorkout.content.isPublic,
+          creator: {
+            id: user.id,
+            name: user.name,
+            image: user.image,
+          },
+          description: "",
+        };
 
-          <div className="buttons">
-            <button
-              className="workout-visibility-toggle"
-              onClick={() => setIsPublic(!isPublic)}
-              data-tooltip-content={isPublic ? "Public" : "Private"}
-              data-tooltip-id="workout-visibility-tooltip"
-              data-tooltip-place="left"
-            >
-              <Icon
-                aria-hidden
-                className="lock"
-                name={isPublic ? "unlock" : "lock"}
-              />
+        onClose(true);
+        newWorkoutsContext.addWorkout(simulatedResponse);
+      });
+    };
 
-              <p aria-hidden={false} className="accessibility-only">
-                {isPublic ? "Public" : "Private"}
-              </p>
+    useEffect(() => {
+      setModalConfirmationOpeningCondition?.(
+        () => validateTitle() || validateSets()
+      );
+    }, [currentSets]);
 
-              <Tooltip id="workout-visibility-tooltip" />
-            </button>
+    return (
+      <CurrentEditingWorkoutSetsContext.Provider
+        value={{
+          currentSets,
+          setCurrentSets,
+        }}
+      >
+        <div ref={wrapperRef} className="workout-creator-container">
+          <div className="header">
+            <input
+              ref={workoutTitleRef}
+              type="text"
+              className="title"
+              placeholder="Workout title"
+              maxLength={25}
+            />
 
-            <button onClick={handleSaveClick} className="save-btn">
-              <p>Save</p>
-              <Icon name="floppy-disk" />
-            </button>
+            <div className="buttons">
+              <button
+                className="workout-visibility-toggle"
+                onClick={() => setIsPublic(!isPublic)}
+                data-tooltip-content={isPublic ? "Public" : "Private"}
+                data-tooltip-id="workout-visibility-tooltip"
+                data-tooltip-place="left"
+              >
+                <Icon
+                  aria-hidden
+                  className="lock"
+                  name={isPublic ? "unlock" : "lock"}
+                />
+
+                <p aria-hidden={false} className="accessibility-only">
+                  {isPublic ? "Public" : "Private"}
+                </p>
+
+                <Tooltip id="workout-visibility-tooltip" />
+              </button>
+
+              <button onClick={() => onClose()} className="discard-btn">
+                <p>Discard</p>
+              </button>
+
+              <button onClick={handleSaveClick} className="save-btn">
+                <p>Save</p>
+                <Icon name="floppy-disk" />
+              </button>
+            </div>
           </div>
-        </div>
 
-        <WorkoutSetCreator
-          onOverlayOpen={() => {
-            if (!wrapperRef.current) return;
+          <WorkoutSetCreator
+            onOverlayOpen={() => {
+              if (!wrapperRef.current) return;
 
-            wrapperRef.current.style.overflow = "hidden";
-            wrapperRef.current.scrollTop = 0;
-          }}
-          onOverlayClose={() => {
-            if (!wrapperRef.current) return;
+              wrapperRef.current.style.overflow = "hidden";
+              wrapperRef.current.scrollTop = 0;
+            }}
+            onOverlayClose={() => {
+              if (!wrapperRef.current) return;
 
-            wrapperRef.current.style.overflow = "auto";
-            wrapperRef.current.scrollTop = 0;
-          }}
-        />
-
-        <div className="description-container">
-          <textarea
-            id="description-input"
-            ref={descriptionTextAreaRef}
-            onChange={(e) => {
-              e.target.style.height = "auto";
-              e.target.style.height = `${e.target.scrollHeight}px`;
+              wrapperRef.current.style.overflow = "auto";
+              wrapperRef.current.scrollTop = 0;
             }}
           />
 
-          <label
-            htmlFor="description-input"
-            className="description-input-label"
-          >
-            Workout description
-          </label>
+          <div className="description-container">
+            <textarea
+              id="description-input"
+              ref={descriptionTextAreaRef}
+              onChange={(e) => {
+                e.target.style.height = "auto";
+                e.target.style.height = `${e.target.scrollHeight}px`;
+              }}
+            />
+
+            <label
+              htmlFor="description-input"
+              className="description-input-label"
+            >
+              Workout description
+            </label>
+          </div>
         </div>
-      </div>
-    </CurrentEditingWorkoutSetsContext.Provider>
-  );
-});
+      </CurrentEditingWorkoutSetsContext.Provider>
+    );
+  },
+  {
+    closeConfirmationModal: {
+      children: (cancel, confirm) => {
+        return (
+          <div className="workout-creator-closing-modal-content-container">
+            <h2 id="workout-creator-closing-modal-title">
+              Are you sure you want to discard your changes?
+            </h2>
+
+            <div className="modal-buttons-container">
+              <button onClick={cancel}>Cancel</button>
+              <button onClick={confirm}>Confirm</button>
+            </div>
+          </div>
+        );
+      },
+      modalProps: {
+        className: {
+          afterOpen: "open",
+          base: "workout-creator-closing-modal",
+          beforeClose: "closing",
+        },
+        overlayClassName: "workout-creator-closing-modal-overlay",
+        closeTimeoutMS: 250,
+        shouldCloseOnOverlayClick: true,
+        shouldFocusAfterRender: true,
+        shouldReturnFocusAfterClose: true,
+        aria: {
+          describedby: "workout-creator-closing-modal-title",
+        },
+      },
+    },
+  }
+);
 
 export default WorkoutCreator;
