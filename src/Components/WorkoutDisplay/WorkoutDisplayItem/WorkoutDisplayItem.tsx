8b96b3c62540621dbf2359b60504a922d9763201
Andrej Nenadić <andrejnana445@gmail.com>
2024-08-25 23:47:38 +0200

    Separate WorkoutDisplayItem's set rendering into a new component ('WorkoutDisplaySet') and better format RiR and volume (rep range)

--- src/Components/WorkoutDisplay/WorkoutDisplayItem/WorkoutDisplayItem.tsx
+++ src/Components/WorkoutDisplay/WorkoutDisplayItem/WorkoutDisplayItem.tsx
@@ -1,7 +1,6 @@
 import "./WorkoutDisplayItem.scss";
 import { Schema } from "../../../Types/Endpoints/SchemaParser";
-import Icon from "../../Icon/Icon";
-import { SetType } from "../../WorkoutSetCreator/WorkoutItem/WorkoutItem";
+import WorkoutDisplaySet from "./WorkoutDisplaySet";
 
 type WorkoutDisplayItemProps = {
   exercise: Schema<"SimpleExerciseResponseDTO">;
@@ -12,13 +11,6 @@ export default function WorkoutDisplayItem({
   sets,
   exercise,
 }: WorkoutDisplayItemProps) {
-  function getIconByType(typeIdx: number, index: number) {
-    const type: SetType = ["1", "w", "d", "f"][typeIdx] as SetType;
-
-    if (type === "1") return <p className="set-icon">{index + 1}</p>;
-    else return <Icon className="set-icon" name={type} />;
-  }
-
   return (
     <div className="workout-display-item">
       <div className="workout-display-item-header">
@@ -37,17 +29,7 @@ export default function WorkoutDisplayItem({
         </div>
 
         {sets.map((set, index) => (
-          <div className="set" key={set.id}>
-            <div>{getIconByType(set.type, index)}</div>
-
-            <div>
-              <p>{set.riR}</p>
-            </div>
-
-            <div>
-              <p>{`${set.bottomRepRange}-${set.topRepRange}`}</p>
-            </div>
-          </div>
+          <WorkoutDisplaySet key={set.id} index={index} set={set} />
         ))}
       </div>
     </div>
