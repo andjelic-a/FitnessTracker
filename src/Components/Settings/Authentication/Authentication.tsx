50e78dea31e5ef5ab5578b1c202f8dddc272f011
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-08-27 18:30:27 +0200

    Add change email and change password to Authentication settings page and style it

--- src/Components/Settings/Authentication/Authentication.tsx
+++ src/Components/Settings/Authentication/Authentication.tsx
@@ -1,4 +1,5 @@
 import SettingsMenu from "../SettingsMenu";
+import InputField from "../../InputField/InputField";
 import "./Authentication.scss";
 
 type EditProfileProps = {
@@ -9,12 +10,54 @@ type EditProfileProps = {
   onClose: () => void;
 };
 
-export default function Authentication({ visible, onClose, setIsEditProfileOpen, setIsAuthenticationOpen, setIsPrivacyOpen }: EditProfileProps) {
+export default function Authentication({
+  visible,
+  onClose,
+  setIsEditProfileOpen,
+  setIsAuthenticationOpen,
+  setIsPrivacyOpen,
+}: EditProfileProps) {
   return (
     <div className={`authentication ${visible ? "authentication-show" : ""}`}>
-      <SettingsMenu onClose={onClose} setIsEditProfileOpen={setIsEditProfileOpen} setIsAuthenticationOpen={setIsAuthenticationOpen} setIsPrivacyOpen={setIsPrivacyOpen}/>
-      <div className="authentication-item">Sign in</div>
-      <div className="authentication-item">Sign up</div>
+      <SettingsMenu
+        onClose={onClose}
+        setIsEditProfileOpen={setIsEditProfileOpen}
+        setIsAuthenticationOpen={setIsAuthenticationOpen}
+        setIsPrivacyOpen={setIsPrivacyOpen}
+      />
+      <div className="authentication-content">
+        <h3>Authentication</h3>
+        <div className="authentication-email">
+          <p>Change Email</p>
+          <InputField
+            maxLength={50}
+            className="authentication-email-input"
+            placeholder="Old email"
+          />
+          <InputField
+            maxLength={50}
+            className="authentication-email-input"
+            placeholder="New email"
+          />
+          <button className="authentication-button">Save</button>
+        </div>
+        <div className="authentication-password">
+          <p>Change Password</p>
+          <InputField
+            maxLength={50}
+            className="authentication-password-input"
+            placeholder="Old password"
+            password
+          />
+          <InputField
+            maxLength={50}
+            className="authentication-password-input"
+            placeholder="New password"
+            password
+          />
+          <button className="authentication-button">Save</button>
+        </div>
+      </div>
     </div>
   );
 }
