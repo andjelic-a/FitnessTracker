061fbee0ccd3ade24f95a9077af9d3450ab40589
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-09-03 23:24:13 +0200

    Add 2fa to Authentication component

--- src/Components/Settings/Authentication/Authentication.tsx
+++ src/Components/Settings/Authentication/Authentication.tsx
@@ -57,6 +57,36 @@ export default function Authentication({
           />
           <button className="authentication-button">Save</button>
         </div>
+        <div className="authentication-2fa">
+          <h3>Two-Factor Authentication</h3>
+          <p>
+            Two-factor authentication (2FA) can be used to help protect your
+            account from unauthorized access. You’ll be required to enter a
+            security code each time you sign in.
+          </p>
+          <div className="authentication-2fa-container">
+            <div className="authentication-2fa-text-area">
+              <h3>Two-Factor Authenticator App</h3>
+              <p>
+                Use an Authenticator App as your two-factor authentication
+                (2FA). When you sign in you’ll be asked to use the security code
+                provided by your Authenticator App.
+              </p>
+            </div>
+          </div>
+          <div className="authentication-2fa-container">
+            <div className="authentication-2fa-icon">
+            </div>
+            <div className="authentication-2fa-text-area">
+              <h3>E-mail For Two-Factor Authenticator</h3>
+              <p>
+                Use the security code sent to your email address as your
+                two-factor authentication (2FA). The security code will be sent
+                to the address associated with your account.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
