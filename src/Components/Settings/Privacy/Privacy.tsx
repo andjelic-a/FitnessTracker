a3e463789bfc6b72c44d4fc2dd1c3f22bf59c9e1
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-09-04 23:25:42 +0200

    Create and style privacy container in Privacy component

--- src/Components/Settings/Privacy/Privacy.tsx
+++ src/Components/Settings/Privacy/Privacy.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SettingsMenu from "../SettingsMenu";
 import "./Privacy.scss";
 
@@ -16,10 +17,37 @@ export default function Privacy({
   setIsAuthenticationOpen,
   setIsPrivacyOpen,
 }: PrivacyProps) {
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
   return (
     <div className={`privacy ${visible ? "privacy-show" : ""}`}>
-      <SettingsMenu setIsEditProfileOpen={setIsEditProfileOpen} setIsAuthenticationOpen={setIsAuthenticationOpen} setIsPrivacyOpen={setIsPrivacyOpen} onClose={onClose} />
-      dsadasdasd
+      <SettingsMenu
+        setIsEditProfileOpen={setIsEditProfileOpen}
+        setIsAuthenticationOpen={setIsAuthenticationOpen}
+        setIsPrivacyOpen={setIsPrivacyOpen}
+        onClose={onClose}
+      />
+      <h3>Privacy</h3>
+      <div className="privacy-container">
+        <div className="privacy-container-text">
+          <h3>Following</h3>
+          <p>
+            You have control over the visibility of your following list. You can
+            choose to make it either public or private. If set to public, anyone
+            who visits your profile can see the accounts you follow.If set to
+            private, your following list will only be visible to you.
+          </p>
+        </div>
+        <div className="privacy-container-button">
+          <div
+            className="privacy-container-button-background"
+            onClick={() => setIsButtonEnabled(!isButtonEnabled)}
+          >
+            <div className={`privacy-container-button-icon ${isButtonEnabled ? "button-enabled" : ""}`}></div>
+            <div className={`privacy-container-button-tail ${isButtonEnabled ? "button-enabled" : ""}`}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
