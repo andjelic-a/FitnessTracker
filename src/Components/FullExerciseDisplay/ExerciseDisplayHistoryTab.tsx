e50fb149e266c0f492dca2340e67e11402fbb6ed
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-20 20:49:15 +0100

    Display exercise history in ExerciseDisplayHistoryTab using LazyLoadingContainer

--- src/Components/FullExerciseDisplay/ExerciseDisplayHistoryTab.tsx
+++ src/Components/FullExerciseDisplay/ExerciseDisplayHistoryTab.tsx
@@ -1,9 +1,48 @@
+import { useContext } from "react";
 import { Schema } from "../../Types/Endpoints/SchemaParser";
+import LazyLoadingContainer from "../LazyLoadingContainer/LazyLoadingContainer";
+import basicProfileInfoContext from "../../Contexts/BasicProfileInfoContext";
 
 type ExerciseDisplayHistoryTabProps = {
   exercise: Schema<"DetailedExerciseResponseDTO">;
 };
 
-export default function ExerciseDisplayHistoryTab({}: ExerciseDisplayHistoryTabProps) {
-  return <div>ExerciseDisplayHistoryTab</div>;
+export default function ExerciseDisplayHistoryTab({
+  exercise,
+}: ExerciseDisplayHistoryTabProps) {
+  const basicInfo = useContext(basicProfileInfoContext);
+
+  return basicInfo === null ? (
+    <div>Login to view exercise history</div>
+  ) : (
+    <LazyLoadingContainer
+      endpoint="/api/exercise/{id}/history"
+      baseAPIRequest={{
+        method: "get",
+        parameters: {
+          id: exercise.id,
+          offset: 0,
+          limit: 10,
+        },
+      }}
+      onSegmentLoad={(segmentData) => {
+        if (segmentData.code !== "OK") return null;
+
+        return segmentData.content.map((completedWorkout) => (
+          <div key={completedWorkout.completionDate}>
+            {completedWorkout.completionDate}
+            {completedWorkout.setsCompleted.map((completedSet, i) => (
+              <div key={i}>
+                {completedSet.weight}x{completedSet.reps}
+              </div>
+            ))}
+          </div>
+        ));
+      }}
+      stopCondition={(segmentData) =>
+        segmentData.code === "Not Found" ||
+        (segmentData.code === "OK" && segmentData.content.length < 10)
+      }
+    />
+  );
 }
