afbb6f526f450b0369189b7e2ce2e8d002b50f8b
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-20 21:13:51 +0100

    Turn ExerciseDisplayHistoryTab into a memo component and add some scss to allow the lazy loading to function

--- src/Components/FullExerciseDisplay/ExerciseDisplayHistoryTab.tsx
+++ src/Components/FullExerciseDisplay/ExerciseDisplayHistoryTab.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { memo, useContext } from "react";
 import { Schema } from "../../Types/Endpoints/SchemaParser";
 import LazyLoadingContainer from "../LazyLoadingContainer/LazyLoadingContainer";
 import basicProfileInfoContext from "../../Contexts/BasicProfileInfoContext";
@@ -7,42 +7,52 @@ type ExerciseDisplayHistoryTabProps = {
   exercise: Schema<"DetailedExerciseResponseDTO">;
 };
 
-export default function ExerciseDisplayHistoryTab({
-  exercise,
-}: ExerciseDisplayHistoryTabProps) {
-  const basicInfo = useContext(basicProfileInfoContext);
+const ExerciseDisplayHistoryTab = memo(
+  ({ exercise }: ExerciseDisplayHistoryTabProps) => {
+    const basicInfo = useContext(basicProfileInfoContext);
 
-  return basicInfo === null ? (
-    <div>Login to view exercise history</div>
-  ) : (
-    <LazyLoadingContainer
-      endpoint="/api/exercise/{id}/history"
-      baseAPIRequest={{
-        method: "get",
-        parameters: {
-          id: exercise.id,
-          offset: 0,
-          limit: 10,
-        },
-      }}
-      onSegmentLoad={(segmentData) => {
-        if (segmentData.code !== "OK") return null;
+    return basicInfo === null ? (
+      <div>Login to view exercise history</div>
+    ) : (
+      <LazyLoadingContainer
+        endpoint="/api/exercise/{id}/history"
+        baseAPIRequest={{
+          method: "get",
+          parameters: {
+            id: exercise.id,
+            offset: 0,
+            limit: 10,
+          },
+        }}
+        onSegmentLoad={(segmentData) => {
+          if (segmentData.code !== "OK") return null;
 
-        return segmentData.content.map((completedWorkout) => (
-          <div key={completedWorkout.completionDate}>
-            {completedWorkout.completionDate}
-            {completedWorkout.setsCompleted.map((completedSet, i) => (
-              <div key={i}>
-                {completedSet.weight}x{completedSet.reps}
-              </div>
-            ))}
-          </div>
-        ));
-      }}
-      stopCondition={(segmentData) =>
-        segmentData.code === "Not Found" ||
-        (segmentData.code === "OK" && segmentData.content.length < 10)
-      }
-    />
-  );
-}
+          return segmentData.content.map((completedWorkout) => (
+            //The outer div represents a completed workout which contains the date of completion and all sets that the user completed of this exercise
+            <div
+              key={completedWorkout.completionDate}
+              className="completed-workout"
+            >
+              <p className="date">{completedWorkout.completionDate}</p>
+
+              {/* Inner div represents a single completed set which contains the reps and weight */}
+              {completedWorkout.setsCompleted.map((completedSet, i) => (
+                <div key={i} className="completed-set">
+                  <p>
+                    {completedSet.weight}x{completedSet.reps}
+                  </p>
+                </div>
+              ))}
+            </div>
+          ));
+        }}
+        stopCondition={(segmentData) =>
+          segmentData.code === "Not Found" ||
+          (segmentData.code === "OK" && segmentData.content.length < 10)
+        }
+      />
+    );
+  }
+);
+
+export default ExerciseDisplayHistoryTab;
