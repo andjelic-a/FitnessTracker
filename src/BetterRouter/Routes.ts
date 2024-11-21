d719eb5afa138ce454862ea633581234029f2a34
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-21 17:07:54 +0100

    Remove all types previously used for unwrapping route objects from Routes.ts

--- src/BetterRouter/Routes.ts
+++ src/BetterRouter/Routes.ts
@@ -1,7 +1,4 @@
 import { LoaderFunctionArgs, ParamParseKey, Params } from "react-router-dom";
-// import { RoutePathObjects } from "../main";
-
-// export type Routes = UnwrappedRoutes<RoutePathObjects>;
 
 interface LoaderArgsInterface<Route extends string> extends LoaderFunctionArgs {
   params: Params<ParamParseKey<Route>>;
@@ -11,45 +8,3 @@ type LoaderArguments<Route extends string> = LoaderArgsInterface<Route>;
 
 export type LoaderParams<Route extends string> =
   LoaderArguments<Route>["params"];
-
-type UnwrappedRoutes<RouteObjects> = RouteObjects extends readonly [
-  infer First,
-  ...infer Rest
-]
-  ?
-      | (First extends {
-          path: infer Path;
-        }
-          ? Path | ParseChildRoutes<First, Path>
-          : never)
-      | UnwrappedRoutes<Rest>
-  : never;
-
-type ParseChildRoutes<RouteObject, Path> = RouteObject extends {
-  children: infer Children;
-}
-  ? ConnectRoutes<UnwrappedRoutes<Children>, Path>
-  : never;
-
-type ConnectRoutes<UnwrappedChildRoutes, ParentRoute> =
-  UnwrappedChildRoutes extends string
-    ? ParentRoute extends string
-      ? `${RemoveTrailingSlash<ParentRoute>}/${RemoveTrailingSlash<UnwrappedChildRoutes>}`
-      : never
-    : never;
-
-type RemoveTrailingSlash<T extends string> = RemoveStartingSlash<
-  RemoveEndingSlash<T>
->;
-
-type RemoveStartingSlash<T extends string> = T extends `/${infer R}`
-  ? R extends `/${infer S}`
-    ? RemoveStartingSlash<S>
-    : R
-  : T;
-
-type RemoveEndingSlash<T extends string> = T extends `${infer R}/`
-  ? R extends `${infer S}/`
-    ? RemoveEndingSlash<S>
-    : R
-  : T;
