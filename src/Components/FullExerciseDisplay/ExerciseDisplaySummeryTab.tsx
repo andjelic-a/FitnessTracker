3eba25ce30ed928c588e4b005e6c2359a541ef08
Aleksa Andjelic <23151811a@gmail.com>
2024-11-22 16:34:47 +0100

    Work on ExerciseDisplaySummeryTab and create OneRepMaxCalculation function

--- src/Components/FullExerciseDisplay/ExerciseDisplaySummeryTab.tsx
+++ src/Components/FullExerciseDisplay/ExerciseDisplaySummeryTab.tsx
@@ -1,5 +1,5 @@
 import { Schema } from "../../Types/Endpoints/SchemaParser";
-import FormattedText from "../FormattedText/FormattedText";
+import Icon from "../Icon/Icon";
 
 type ExerciseDisplaySummeryTabProps = {
   exercise: Schema<"DetailedExerciseResponseDTO">;
@@ -8,52 +8,87 @@ type ExerciseDisplaySummeryTabProps = {
 export default function ExerciseDisplaySummeryTab({
   exercise,
 }: ExerciseDisplaySummeryTabProps) {
+
+  const OneRepMaxCalculation = (weight: number, reps: number) => {
+    return Math.abs(weight / ((1.0278 - reps * 0.0278)));
+  };
+
+  const oneRepMax =
+    exercise.mostWeightLifted?.weight && exercise.mostWeightLifted?.reps
+      ? OneRepMaxCalculation(
+          exercise.mostWeightLifted?.weight,
+          exercise.mostWeightLifted?.reps
+        )
+      : 0;
+
   return (
     <>
       <div className="full-exercise-display-info">
         <h1>{exercise.name}</h1>
-        <div className="full-exercise-display-info-description">
-          <FormattedText children={exercise.description} />
+
+        <div className="full-exercise-display-info-muscle-equipment-section">
+          <div className="full-exercise-display-info-muscle-equipment">
+            <h3>Primary Muscles:</h3>
+            &nbsp;
+            <p>
+              {exercise.primaryMuscles.length > 0
+                ? exercise.primaryMuscles
+                    .map((muscle) => muscle.name)
+                    .join(", ")
+                : "none"}
+            </p>
+          </div>
+
+          <div className="full-exercise-display-info-muscle-equipment">
+            <h3>Secondary Muscles:</h3>
+            &nbsp;
+            <p>
+              {exercise.secondaryMuscles.length > 0
+                ? exercise.secondaryMuscles
+                    .map((muscle) => muscle.name)
+                    .join(", ")
+                : "none"}
+            </p>
+          </div>
+
+          <div className="full-exercise-display-info-muscle-equipment">
+            <h3>Equipment:</h3>
+            &nbsp;
+            <p>
+              {exercise.equipment.length > 0
+                ? exercise.equipment
+                    .map((equipment) => equipment.name)
+                    .join(", ")
+                : "none"}
+            </p>
+          </div>
         </div>
 
-        <h2>Primary Muscles</h2>
-        <ul>
-          {exercise.primaryMuscleGroups.map((muscleGroup) => (
-            <li key={muscleGroup.id}>
-              <h3>{muscleGroup.name}</h3>
-              {exercise.primaryMuscles
-                .filter((muscle) => muscle.muscleGroupId === muscleGroup.id)
-                .map((muscle) => (
-                  <p key={muscle.id}>{muscle.name}</p>
-                ))}
-            </li>
-          ))}
-        </ul>
+        <div className="full-exercise-display-info-personal-records">
+          <div className="full-exercise-display-info-personal-records-header">
+            <Icon name="award" className="icon" />
+            <h3>Personal Records</h3>
+          </div>
 
-        {exercise.secondaryMuscleGroups.length > 0 && (
-          <>
-            <h2>Secondary Muscles</h2>
-            <ul>
-              {exercise.secondaryMuscleGroups.map((muscleGroup) => (
-                <li key={muscleGroup.id}>
-                  <h3>{muscleGroup.name}</h3>
-                  {exercise.secondaryMuscles
-                    .filter((muscle) => muscle.muscleGroupId === muscleGroup.id)
-                    .map((muscle) => (
-                      <p key={muscle.id}>{muscle.name}</p>
-                    ))}
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-
-        <h2>Equipment</h2>
-        <ul>
-          {exercise.equipment.map((equipment) => (
-            <li key={equipment.id}>{equipment.name}</li>
-          ))}
-        </ul>
+          <div className="full-exercise-display-info-personal-records-body">
+            <div className="full-exercise-display-info-personal-body-record">
+              <p>Heaviest Weight</p>
+              <p>{exercise.mostWeightLifted?.weight || 0 + "kg"}</p>
+            </div>
+            <div className="full-exercise-display-info-personal-body-record">
+              <p>Best 1RM</p>
+              <p>{oneRepMax + "kg"}</p>
+            </div>
+            <div className="full-exercise-display-info-personal-body-record">
+              <p>Best Set Volume</p>
+              <p>{(exercise.mostVolumeLifted?.weight || 0) * (exercise.mostVolumeLifted?.reps || 0) + "kg"}</p>
+            </div>
+            <div className="full-exercise-display-info-personal-body-record">
+              <p>Best Session Volume</p>
+              <p>{"kg"}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
